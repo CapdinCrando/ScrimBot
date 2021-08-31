af4944b9071658aca7fe27061e5fc90a52c9bb1b
Tristan Jay <capdincrando@gmail.com>
2021-08-31 11:06:06 -0500

    Api cleanup

--- quoteApi.py
+++ quoteApi.py
@@ -4,12 +4,8 @@ from random import choice
 class QuoteApi:
 	def __init__(self, key, base_id):
 		self.table = Table(key, base_id, 'Quotes')
-	def get_all_quotes(self):
-		return self.table.all()
-	def get_quote(self):
-		return choice(self.get_all_quotes())
 	def get_formatted_quote(self):
-		fields = self.get_quote()['fields']
+		fields = choice(self.table.all())['fields']
 		quote = fields['Quote']
 		if 'Author' in fields:
 			quote += '\n\t- ' + fields['Author']
